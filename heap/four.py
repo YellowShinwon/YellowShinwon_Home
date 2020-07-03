15c45eeb716660a8079717be48285d061cb634d6
YellowShinwon <rea246@snu.ac.kr>
2020-07-03 17:22:04 +0900

    7/3

--- heap/four.py
+++ heap/four.py
@@ -1,10 +1,55 @@
-operations	= ['I 16','D 1']
+# operations	= ['I 16','D 1']
 # return = [0,0]
-operations = ['I 7','I 5','I -5','D -1']
+operations = ['I 7','I 6','I -5','D -1']
 # return = [7,5]
 
+import heapq
 def solution(operations):
-    for command in operations:
-        command, code = command.split()
+    data = []
+    direction = -1
+    for element in operations:
+        command, code = element.split()
+        code = int(code)
+        if command == 'I':
+            heapq.heappush(data, code)
+        else:
+            try:
+                direction, data = delete_max_min(code, direction, data)
+            except(IndexError):
+                pass
+    try:
+        answer = return_max_min(direction, data)
+    except(IndexError):
+        answer = [0,0]
+    return answer
 
-solution(operations)
+def convert_max_min(data):
+    data = [-number for number in data]
+    heapq.heapify(data)
+    return data
+
+def delete_max_min(command, direction, data):
+    if data == []:
+        return direction
+    if command != direction:
+        data = convert_max_min(data)
+        heapq.heappop(data)
+        direction = command
+    else:
+        heapq.heappop(data)
+    return direction, data
+
+def return_max_min(direction, data):
+    if direction == 1:
+        max = (-1)*heapq.heappop(data)
+        data = convert_max_min(data)
+        min = heapq.heappop(data)
+    else:
+        min = heapq.heappop(data)
+        data = convert_max_min(data)
+        max = (-1)*heapq.heappop(data)
+    return max, min
+
+
+hey = solution(operations)
+print(f'answer={hey}')
